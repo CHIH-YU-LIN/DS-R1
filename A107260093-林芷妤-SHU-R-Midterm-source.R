a9586d53b558b1df18fe42840c75c3e30cd1c576
CHIH-YU-LIN <61856998+CHIH-YU-LIN@users.noreply.github.com>
2020-12-11 12:05:12 +0800

    Add files via upload

---
+++ A107260093-林芷妤-SHU-R-Midterm-source.R
@@ -0,0 +1,52 @@
+# 2020/12/11(五), 109學年第一學期 資料科學應用 R期中考
+#
+# 學號:A107260093   姓名:林芷妤
+#
+# 本檔案為各題之程式碼檔，無執行結果
+# ex1
+study <- function(x){
+  Tuition <- 400X+600Y
+   
+}
+data.frame()
+
+
+# ex2(a)
+library(readxl)
+Score <- read_excel("data/Score-109.xlsx", skip=2)
+head(Score, 5)
+tail(Score, 5)
+str(Score)
+
+# ex2(b)
+
+Score <- sample(0:75, NA=0)& any(x < 60)
+data.frame(Score)
+
+# ex2(c)
+
+cor(Score)
+cor <- as.numeric
+cor()
+cor <- my.cor(Score)
+my.cor()
+
+# ex2(d)
+
+# ex3(a)
+
+my.dnorm <- function(x){
+  #µ <- 0
+  #σ <- 1
+   µ <- mean(x)
+   σ <- sd(x)
+}
+
+my.dnorm(2.5, 3, 2)
+
+dnorm(2.5, 3, 2)
+
+# ex3(b)
+
+data.frame(my dnorm, dnorm)
+  
